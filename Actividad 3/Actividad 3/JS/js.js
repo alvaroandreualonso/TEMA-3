b2b638b9429b017e603ab5d968622532c362bf23
alvaroandreualonso <151035794+alvaroandreualonso@users.noreply.github.com>
2024-02-09 11:44:11 +0100

    Add files via upload

---
+++ Actividad 3/Actividad 3/JS/js.js
@@ -0,0 +1,6 @@
+function pedirDatos(){
+    let edad = prompt("¿Cúantos años tienes?");
+    let nombre = prompt("¿Cómo te llamas?");
+    alert ("Hola " + nombre +", veo que tienes" + edad + "años.");
+    
+}
